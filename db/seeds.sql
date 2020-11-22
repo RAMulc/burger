02dde515fbe5074249caef7d44ccf45241b3c2a4
Rob <ramulcrone@gmail.com>
2020-11-22 19:38:06 +0800

    Initial working local version

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Plain & Simple');
+INSERT INTO burgers (burger_name) VALUES ('Cheeese Cheese Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Raw and Wild');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Deelite');
